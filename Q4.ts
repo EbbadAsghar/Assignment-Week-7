c748452fe93d47dd7f2f02ce6b95ed290f0299fe
Ebbad Asghar <ebbadasghar@gmail.com>
2023-12-23 19:45:20 +0500

    Q4

---
+++ Q4.ts
@@ -0,0 +1,6 @@
+// Write a program that uses a for loop to print the first 10 even numbers.
+for (let i = 2; i <= 20; i++) {
+  if (i % 2 == 0) {
+    console.log(`${i} It's an Even Number.`);
+  }
+}
